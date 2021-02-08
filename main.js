ff553b54322c36a319ff5ad76f9071e020cafe9c
celebert <57358979+celebert@users.noreply.github.com>
2021-02-08 22:29:05 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 document.getElementById("playBox").style.display="none";
 document.getElementById("score").style.display="none";
 document.getElementById("instructions").style.display="none";
+document.getElementById("scoreTwo").style.display="none";
+document.getElementById("instructionsTwo").style.display="none";
 document.getElementById("retry").style.display="none";
+document.getElementById("board").style.display="none";
 
 function playGame(url){
   var head = document.getElementsByTagName('head')[0];
@@ -14,5 +17,8 @@ function playGame(url){
   document.getElementById("playBox").style.removeProperty('display');
   document.getElementById("score").style.removeProperty('display');
   document.getElementById("instructions").style.removeProperty('display');
+  document.getElementById("scoreTwo").style.removeProperty('display');
+  document.getElementById("instructionsTwo").style.removeProperty('display');
   document.getElementById("retry").style.removeProperty('display');
+  document.getElementById("board").style.removeProperty('display');
 }
